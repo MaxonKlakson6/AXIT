b7387dfdde1aa6f6d07b57212560089abe6a38f4
Maxim <mihalenko.maxim21@gmail.com>
2022-04-14 12:52:55 +0300

    Adapted project for tablets and phones

--- script.js
+++ script.js
@@ -9,3 +9,25 @@ const chooseActiveTab = (tab) => {
     document.querySelector('#C' + id).classList.remove('hidden');
     document.querySelector('#i' + id).classList.remove('hidden');
 }
+
+const changeMenuState = (btn) => {
+    const menu = document.querySelector('.nav-bar__menu');
+    const links = document.querySelectorAll('.link');
+    let isActive = btn.style.backgroundImage.toString();
+
+    if(isActive == 'url("images/header/close.svg")'){
+        btn.style.backgroundImage = "url('images/header/menu.svg')";
+        menu.style.display = 'none';
+        links.forEach((item) => item.style.display = 'none');
+        return;
+    }
+
+    btn.style.backgroundImage = 'url("images/header/close.svg")';
+    menu.style.display = 'flex';
+    links.forEach((item) => item.style.display = "block");
+}
+
+let item = document.querySelector('.customers__item');
+let move = item.offsetWidth;
+const itemContainer = document.querySelector('.customers__list');
+itemContainer.style.transform = `trasnlateX:${move}px`;
